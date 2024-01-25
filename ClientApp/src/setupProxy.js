38d9da5ad0e31cb227e24d21e38cc7cfebf275d2
mr.phatsorn <mr.phatsorn@gmail.com>
2024-01-25 09:39:45 +0700

    swagger UI the frist

--- ClientApp/src/setupProxy.js
+++ ClientApp/src/setupProxy.js
@@ -5,6 +5,7 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
   env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:9554';
 
 const context = [
+  "/swagger",
   "/weatherforecast",
 ];
 
--- Program.cs
+++ Program.cs
@@ -4,20 +4,25 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddControllers();
+// builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+/* builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("V1",
+    new() {Title="Fritz's Contacts API", Version="1"});
+}); */
 
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
